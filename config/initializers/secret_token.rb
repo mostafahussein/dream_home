8aa17efe1246076b25af75150cfb6ad93ed89b9b
’code-vortex’ <darkbrotherhood.assassin@gmail.com>
2013-04-13 17:19:26 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'dad313979a01a72727eea454a40c9e0bc65034fe49d3db0ed2051f6d4cc600b70e90f12c9a74a5b09c930dc94a5069cd86865241d2bf4446805a090b816f5299'
