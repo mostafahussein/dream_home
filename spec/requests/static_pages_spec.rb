6e9f1e3f35c1007ddb4b0c56326828ac0bcd68dc
’code-vortex’ <darkbrotherhood.assassin@gmail.com>
2013-04-14 20:43:41 +0200

    Add a title Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,17 @@ describe "Static pages" do
 
 		it "should have the h1 'Home'" do
 		visit '/static_pages/home'
-		page.should have_selector('h1' , :text => 'Home')
+		page.should have_selector('h1' , :text => 'Sample App')
 		end
 		
-		it "should have the right title" do
+		it "should have the base title" do
 		visit '/static_pages/home'
-		page.should have_selector('title' , :text => 'Home | By Code Vortex')
+		page.should have_selector('title' , :text => "By Code Vortex")
+		end
+		
+		it "should not have a custom page title" do
+		visit '/static_pages/home'
+		page.should_not have_selector('title' , :text => '| Home')
 		end
 	end
 
@@ -24,7 +29,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 		visit '/static_pages/help'
-		page.should have_selector('title' , :text => 'Help | By Code Vortex')
+		page.should have_selector('title' , :text => "Help | By Code Vortex")
 		end
 	end
 
@@ -37,7 +42,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 		visit '/static_pages/about'
-		page.should have_selector('title' , :text => 'About Us | By Code Vortex')
+		page.should have_selector('title' , :text => "About Us | By Code Vortex")
 		end
 	end
 
@@ -50,7 +55,7 @@ describe "Static pages" do
 
 		it "should have the right title" do
 		visit '/static_pages/contact'
-		page.should have_selector('title' , :text => 'Contact | By Code Vortex')
+		page.should have_selector('title' , :text => "Contact | By Code Vortex")
 		end
 	end
 
