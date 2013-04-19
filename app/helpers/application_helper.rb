ebf7953bcd491a401f223e6de1ecde125796ff98
’code-vortex’ <darkbrotherhood.assassin@gmail.com>
2013-04-19 07:30:54 +0200

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
       def full_title(page_title)
        base_title = 'By Code Vortex'
        if page_title.empty?
-	base_title
+  	     base_title
        else
-	"#{page_title} | #{base_title}"
+	      "#{page_title} | #{base_title}"
        end
      end
 end
